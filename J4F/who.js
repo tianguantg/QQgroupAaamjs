22b29a94dcdd67abf66f1f95c13bca9783d675d2
tg <2519521987@qq.com>
2026-04-09 17:50:54 +0800

    拆分省份等字段

--- J4F/who.js
+++ J4F/who.js
@@ -1,18 +1,16 @@
 const APIs=[
     {name:'IPIP.NET',url:'https://myip.ipip.net',t:'text',f:d=>{
         const m=d.match(/IP[：:]\s*(\S+).*?来自于[：:]\s*(.+)/);
-        if(!m)return{ip:'-',loc:'-',isp:'-'};
+        if(!m)return{ip:'-',country:'-',region:'-',city:'-',isp:'-'};
         const p=m[2].split(/\s+/).filter(Boolean);
-        return{ip:m[1],loc:p.slice(0,3).join(' '),isp:p[3]||'-'}
+        return{ip:m[1],country:p[0]||'-',region:p[1]||'-',city:p[2]||'-',isp:p[3]||'-'}
     }},
-    {name:'IP.sb',url:'https://api.ip.sb/geoip',t:'json',f:d=>{
-        const loc=[d.country,d.region].filter(Boolean).join(' ');
-        return{ip:d.ip,loc:loc,isp:d.isp||d.organization||'-'}
-    }},
-    {name:'myip.la',url:'https://api.myip.la/cn?json',t:'json',f:d=>{
-        const loc=[d.location?.province,d.location?.city].filter(Boolean).join(' ');
-        return{ip:d.ip,loc:loc||'-',isp:'-'}
-    }}
+    {name:'IP.sb',url:'https://api.ip.sb/geoip',t:'json',f:d=>({
+        ip:d.ip,country:d.country||'-',region:d.region||'-',city:d.city||'-',isp:d.isp||d.organization||'-'
+    })},
+    {name:'myip.la',url:'https://api.myip.la/cn?json',t:'json',f:d=>({
+        ip:d.ip,country:d.location?.country_name||'-',region:d.location?.province||'-',city:d.location?.city||'-',isp:'-'
+    })}
 ];
 
 function query(){
@@ -29,9 +27,11 @@ function query(){
             div.querySelector('.status').className='status ok';
             div.querySelector('.status').textContent='成功';
             div.querySelector('.body').innerHTML=`
-                <div class="row"><span class="k">IP</span><span class="v">${x.ip||'-'}</span></div>
-                <div class="row"><span class="k">位置</span><span class="v">${x.loc||'-'}</span></div>
-                <div class="row"><span class="k">运营商</span><span class="v">${x.isp||'-'}</span></div>
+                <div class="row"><span class="k">IP</span><span class="v">${x.ip}</span></div>
+                <div class="row"><span class="k">国家</span><span class="v">${x.country}</span></div>
+                <div class="row"><span class="k">省份</span><span class="v">${x.region}</span></div>
+                <div class="row"><span class="k">城市</span><span class="v">${x.city}</span></div>
+                <div class="row"><span class="k">运营商</span><span class="v">${x.isp}</span></div>
                 <pre>${typeof d=='string'?d:JSON.stringify(d,null,2)}</pre>
             `;
         }).catch(e=>{
